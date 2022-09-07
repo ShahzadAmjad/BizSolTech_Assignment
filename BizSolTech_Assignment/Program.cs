46abcb40adfa88c6b1c02a5edba40b88f2331edb
Administrator <Administrator@DESKTOP-69IU7BQ>
2022-09-07 17:53:52 +0500

    update

--- BizSolTech_Assignment/Program.cs
+++ BizSolTech_Assignment/Program.cs
@@ -17,12 +17,7 @@ builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 builder.Services.AddScoped<IDeveloperRepository, SQLDeveloperRepository>();
 
 
-
 var app = builder.Build();
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
