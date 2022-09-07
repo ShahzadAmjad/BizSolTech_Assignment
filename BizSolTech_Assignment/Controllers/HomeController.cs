930155896a8c2bcf774200c21e5dae165ff4ec15
Shehzad Amjad <74287706+ShahzadAmjad@users.noreply.github.com>
2022-09-07 15:43:41 +0500

    add developer complete

--- BizSolTech_Assignment/Controllers/HomeController.cs
+++ BizSolTech_Assignment/Controllers/HomeController.cs
@@ -13,11 +13,39 @@ namespace BizSolTech_Assignment.Controllers
         [HttpGet]
         public ViewResult Index()
         {
-            var model = _developerRepository.GetAllDevelopers;
-
+            IEnumerable<Developer> model = _developerRepository.GetAllDevelopers();
             return View("AllDevelopers", model);
+        }
 
-            //return View("AllDevelopers");
+        //[HttpGet]
+        //public IActionResult AddPropertySchedule()
+        //{
+        //    PropertySchedule propertySchedule = new PropertySchedule();
+        //    return PartialView("_AddPropertySchedulePartialView", propertySchedule);
+        //}
+        //[HttpPost]
+        //public IActionResult AddPropertySchedule(PropertySchedule propertySchedule)
+        //{
+
+        //}
+
+        [HttpGet]
+        public IActionResult AddDeveloper()
+        {
+            Developer developer = new Developer();
+            return PartialView("_AddDeveloperPartialView", developer);
+        }
+
+        [HttpPost]
+        public IActionResult AddDeveloper(Developer developer)
+        {
+           // if(ModelState.IsValid)
+            {
+                _developerRepository.AddDeveloper(developer);
+            }
+
+            IEnumerable<Developer> model = _developerRepository.GetAllDevelopers();
+            return View("AllDevelopers", model);
         }
     }
 }
